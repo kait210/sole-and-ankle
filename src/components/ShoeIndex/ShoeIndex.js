f12ab79496b0782ce1b98664866823b6ebcdb46b
Kait Beaudette <3999983+kait210@users.noreply.github.com>
2022-03-29 15:55:14 -0400

    Update ShoeGrid

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -25,7 +25,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        {/* <ShoeGrid /> */}
+        <ShoeGrid />
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -60,6 +60,7 @@ const LeftColumn = styled.div`
 const MainColumn = styled.div`
   {
     display: flex;
+    flex-direction: column;
     flex: 1;
   }
 `;
