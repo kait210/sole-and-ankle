ca1bf91e5682d09c195ac0c26f7b5680b81c78cf
Kait Beaudette <3999983+kait210@users.noreply.github.com>
2022-03-29 14:01:11 -0400

    update Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,19 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: center;
+  height: 72px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  {
+    display: flex;
+    justify-content: center;
+    gap: 48px;
+    flex: 1;
+  }
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
